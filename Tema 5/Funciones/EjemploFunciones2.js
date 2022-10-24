9ea241f7a58c59b743a9d6a6fa05f4c93194e087
manel <manelborras21@gmail.com>
2022-10-24 20:11:08 +0200

    modified:   Tema 5/Funciones/EjemploFunciones2.js

--- Tema 5/Funciones/EjemploFunciones2.js
+++ Tema 5/Funciones/EjemploFunciones2.js
@@ -1,7 +1,15 @@
-function f(){
-    const a=9;
-    let b=9;
-    var c=9;
+if (true) {
+    var c = 9;
+    let b = 9;
+}
+
+function f() {
+    const a = 9;
+    b = 10;
+    c = 10;
     console.log("Soy la función f");
-    }
-    f();
+}
+
+f();
+
+console.log(b);
